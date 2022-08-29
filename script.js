fc2247a48b3d1c21d1c26eb50321404278865118
Philip Sjøtlov <philipsjoetlov@gmail.com>
2022-08-29 15:40:30 +0200

    Indført data i browser done!

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const url = "https://babushka-dd8a.restdb.io/rest/menu";
 const apiKey = {
-  header: {
+  headers: {
     "x-apikey": "600ec2fb1346a1524ff12de4",
   },
 };
@@ -12,12 +12,12 @@ async function hentData() {
 }
 
 const section = document.querySelector("section");
-const template = document.querySelector("template").content;
+const temp = document.querySelector("template").content;
 
 function vis(json) {
   json.forEach((ret) => {
     const klon = temp.cloneNode(true);
-    klon.querySelector(".billede").src = "faces/" + ret.billede;
+    klon.querySelector(".billede").src = "medium/" + ret.billede;
     klon.querySelector(".ret").textContent = ret.navn;
     klon.querySelector(".info").textContent = ret.titel;
     klon.querySelector(".pris").textContent = ret.pris + "kr.";
